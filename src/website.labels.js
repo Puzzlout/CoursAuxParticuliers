f0b7b9c234e6c646046d168e6ae3f44132a1e29c
Jeremie Litzler <jeremie.litzler@conduent.com>
2019-07-11 16:58:23 +0200

    feat: put labels in javascript object

---
+++ src/website.labels.js
@@ -0,0 +1,41 @@
+var labels = {
+    copyright: "Puzzlout, tous droits réservés © 2019",
+    author: "Aurélie Litzler",
+    pageTitle: "Cours aux particuliers",
+    pageDescription: "Piano, Flûte, Maths, Anglais dans les environs de Tournon-sur-Rhône et Tain l'Hermitage.",
+    menusItems: [
+        "Professeur",
+        "Cours",
+        "Avis",
+        "Contact"
+    ],
+    sectionTop: {
+        introText: "Bonjour. Je m'appelle",
+        firstName: "Aurélie",
+        description: "Je donne des cours de <strong>Musique, Soutien scolaire et Anglais</strong> à tous les niveaux.",
+        buttonText: "Réserver un cours"
+    }
+    sectionCourses: {
+        title: "Voici le détail des cours que je donne.",
+        courses: [
+            { 
+                title: "Musique", 
+                description: "Ornare nulla proin odio consequat sapien vestibulum ipsum primis sed amet consequat lorem dolore."
+            },
+            {
+                title: "Soutien scolaire", 
+                description: "Ornare nulla proin odio consequat sapien vestibulum ipsum primis sed amet consequat lorem dolore."
+            },
+            {
+                title: "Anglais", 
+                description: "Ornare nulla proin odio consequat sapien vestibulum ipsum primis sed amet consequat lorem dolore."
+            }
+        ]
+    },
+    sectionReviews: {},
+    sectionContact: {
+        title: "Vous avez besoin de moi ?",
+        description: "Remplissez le formulaire ci-dessous, SVP.",
+    }
+
+}
